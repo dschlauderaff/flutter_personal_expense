30149959e2dac682c55c90331e65777d099dcd12
Dave Schlauderaff <dschlaud@gmail.com>
2020-02-14 16:59:27 -0600

    extract button onPressed to a function, call function from keyboard submit, added validation

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,16 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTransaction);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTransaction(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,6 +29,8 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
+
 //                    onChanged:  (value) {
 //                      titleInput = value;
 //                    },
@@ -26,17 +38,13 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
-//                    onChanged: (value) => amountInput = value,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () {
-                addTransaction(
-                    titleController.text,
-                    double.parse(amountController.text)
-                );
-              },
+              onPressed: submitData,
             )
           ],
         ),
