3bdbb43fca6a235f4ea6274145966ce274f1be4c
Dave Schlauderaff <dschlaud@gmail.com>
2020-02-17 15:21:30 -0600

    padding and format changes

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -40,14 +40,21 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
 
-        return ChartBar(
-            data['day'],
-            data['amount'],
-            totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending);
-        }).toList(),
+          return Flexible(
+            fit: FlexFit.tight,
+            child: ChartBar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending),
+          );
+          }).toList(),
+        ),
       ),
     );
   }
